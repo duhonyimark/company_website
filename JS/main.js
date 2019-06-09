af9c7f123917af6cdf6a08d93ad74e6ce989bf3e
Mark Duhonyi <duhonyimark@gmail.com>
2019-06-09 16:46:53 +0100

    compnay website

---
+++ JS/main.js
@@ -0,0 +1,38 @@
+// Initialize and add the map
+function initMap () {
+    const loc = {lat: 51.507351, lng: -0.127758};
+// Centered map on location
+    const map = new google.maps.Map(document.querySelector('.map')
+    , {
+        zoom: 14,
+        center: loc
+    });
+// The marker, positioned at location
+    const marker = new google.maps.Map ({position: loc, map:map});
+}
+
+// Sticky menu background
+window.addEventListener('scroll', function() {
+    if (window.scrollY > 150) {
+        document.querySelector('#navbar').style.opacity = 0.9;
+    } else {
+        document.querySelector('#navbar').style.opacity = 1;
+    }
+});
+
+
+// Smooth scrolling
+$('#navbar a, .btn').on('click', function(event) {
+    if (this.hash !== '') {
+        event.preventDefault();
+
+        const hash = this.hash;
+
+        $('html, body').animate(
+            {
+                scrollTop: $(hash).offset().top - 100
+            },
+            800
+        );
+    }
+});
